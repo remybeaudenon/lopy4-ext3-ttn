a2c56fb53ab3b6850bee07290ed8210a78434993
Remy Beaudenon <remybeaudenon@yahoo.com>
2023-01-16 18:23:32 +0100

    fixes

--- main.py
+++ main.py
@@ -30,10 +30,6 @@ while not lorawan.has_joined() :
 led.setState(LED.GREEN)   
 LOGGER.log('MAIN:main()','Loop Started !!!'  ) 
 
-delta_temp = 0.5 # ° 
-temp_last = 0 
-default_last = 0
-
 ping_activity   = 0 
 ping_delay      = 3600  
 
@@ -43,29 +39,18 @@ sleep_delay = 10
 try :
     while True :
 
+        # Skip if not ready 
         if not sensor1.isReady() : 
             time.sleep(sleep_delay)
             led.setState(LED.YELLOW) 
             continue
-        # Application Logic 
-        payload = sensor1.getPayload()
-        temp_value = payload.get('temp')
-        default = payload.get('default')
 
-        # 'E'vent on default 
-        if ( default ^ default_last ) :
-            default_last = default
-            LOGGER.log('MAIN:main()','New push default: {}'.format(default) ) 
+        #  Process sensor activities 
+        payload  = sensor1.process()
+        if isinstance(payload, dict) :    
             # Push data 
             lorawan.send(LM335A.getHexPayload(payload))
             ping_activity = 0 
-        elif abs(temp_value - temp_last)  > delta_temp :
-            temp_last = temp_value 
-            LOGGER.log('MAIN:main()','Sensor new push value: {}'.format(temp_value) )
-            # Push data  
-            lorawan.send(LM335A.getHexPayload(payload))
-            ping_activity = 0 
-    
         elif ping_activity > ping_delay : 
             payload['event'] = 'P'
             LOGGER.log('MAIN:main()','Sensor new push "Ping activity" : {}'.format(payload) )
@@ -74,7 +59,7 @@ try :
             ping_activity = 0 
     
         else :
-            LOGGER.log('MAIN:main()','Sensor temp. old:{} new:{}'.format(temp_last,temp_value) ) 
+            LOGGER.log('MAIN:main()','Sensor activity: {}'.format(payload) ) 
             ping_activity += sleep_delay
             time.sleep(sleep_delay)
 
